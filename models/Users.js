52c85f71d3db98d52a5a85c952cfc996663fe2f4
Eric Chang <e4chang@ucsd.edu>
2017-05-05 20:00:30 -0700

    Changed encryption to bcrypt

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var crypto = require('crypto');
+var bcrypt = require('bcrypt');
 
 var UsersSchema = new mongoose.Schema({
   username: {type: String, lowercase: true, unique: true},
@@ -11,14 +11,14 @@ var UsersSchema = new mongoose.Schema({
 });
 
 UsersSchema.methods.setPassword = function(password) {
-  this.salt = crypto.randomBytes(16).toString('hex');
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  // Generate salt
+  this.salt = bcrypt.genSaltSync();
+  this.hash = bcrypt.hashSync(password, this.salt);
 };
 
 UsersSchema.methods.checkPassword = function(password) {
-  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
-
+  var hash = bcrypt.hashSync(password, this.salt);
   return this.hash == hash;
-}
+};
 
 mongoose.model('Users', UsersSchema);
