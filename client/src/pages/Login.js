7104b57c068cbd4dc6c629d33525e033e9e7ae5d
Eric Chang <e4chang@ucsd.edu>
2017-05-05 19:14:24 -0700

    Form submit button

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  handleSubmit() {
+    alert('hello');
+  }
   render() {
     return (
       <div className="uk-grid">
         <div className="uk-width-1-3"></div>
         <div className="uk-width-1-3">
           <div className="uk-card uk-card-default uk-card-body">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <h2>Login</h2>
               <label> Username: </label>
               <input type="text" className="uk-input" name="username" />
