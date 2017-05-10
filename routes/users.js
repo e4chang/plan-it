6cd07102b186a62e57a1af797ea66a1b9515dda8
Eric Chang <e4chang@ucsd.edu>
2017-05-09 18:37:46 -0700

    API for getting trips for user

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,39 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var Trips = mongoose.model('Trips');
+var Users = mongoose.model('Users');
 var router = express.Router();
 
 /* GET Test authorization. */
 router.get('/', function(req, res, next) {
   res.send('Authorization passed.');
+});
+
+/* GET Trips for user */
+router.get('/trips/:id', (req, res, next) => {
+  /* First get the username by id */
+  var user_id = mongoose.Types.ObjectId(req.params.id);
+  var username = Users.findOne({_id: user_id},
+    (err, user) => {
+      if(err) {
+        return res.json({success: false});
+      }
+      if(!user) {
+        console.log('User not found');
+      }
+
+      /* Get all trips for created by this user */
+      var result = Trips.find({owner: user.username},
+        (err, trips) => {
+          if(err) {
+            return res.json({success: false});
+          }
+          if(!trips) {
+            console.log('No trips not found');
+          }
+          return res.json(trips);
+      });
+  });
 });
 
 /* POST Create trip. */
